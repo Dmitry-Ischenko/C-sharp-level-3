ba14298e94194036c3362f02710aa4dd9939265f
dmitry <dmitry@dtech-ural.ru>
2020-09-23 13:04:00 +0500

    Подключил библиотеки Net.Mail Релазиовал поведение кнопки от править и очистить

--- MailSender/MailSender/MainWindow.xaml.cs
+++ MailSender/MailSender/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Net;
+using System.Net.Mail;
+using System.Drawing;
+using System.Xml.Schema;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MailSender
 {
@@ -24,10 +17,81 @@ namespace MailSender
         {
             InitializeComponent();
         }
+        private void StatusBarSendMessage(string StrginMessage, Brush color)
+        {
+            StatusBarMessage.Text = StrginMessage;
+            StatusBarMessage.Foreground = color;
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (Server.Text.Length == 0)
+            {
+                StatusBarSendMessage("Не указан адрес сервера SMTP", Brushes.Red);
+                return;
+            }
+            if (Login.Text.Length == 0)
+            {
+                StatusBarSendMessage("Не указан логин", Brushes.Red);
+                return;
+            }
+            if (port.Text.Length == 0)
+            {                
+                StatusBarSendMessage("Не указан порт SMTP Сервера", Brushes.Red);
+                return;
+            }
+            if (SendTo.Text.Length == 0)
+            {
+                StatusBarSendMessage("Не указано имя получателя сообщения", Brushes.Red);
+                return;
+            }
+            int _port;
+            if (!Int32.TryParse(port.Text, out _port))
+            {
+                StatusBarSendMessage("Не верно указан порт SMTP сервера", Brushes.Red);
+                return;
+            }
+            var client = new SmtpClient(Server.Text,_port);
+            var from = new MailAddress(Login.Text);
+            var to = new MailAddress(SendTo.Text);
+            var message = new MailMessage(from, to);
+            message.Subject = Subject.Text;
+            message.Body = Message.Text;
+            switch (SecureType.SelectedIndex)
+            {
+                case 0:
+                    {
+                        client.EnableSsl = true;
+                        break;
+                    }
+            }
+            client.Credentials = new NetworkCredential
+            {
+                UserName = Login.Text,
+                SecurePassword = Password.SecurePassword
+            };
+            try
+            {
+                client.Send(message);
+                StatusBarSendMessage("Сообщение вроде отправленно", Brushes.Black);
+                SendTo.Text = "";
+                Message.Text = "";
+                Subject.Text = "";
+            } catch
+            {
+                StatusBarSendMessage("Все сломалось, обратитесь к разработчику", Brushes.Red);
+            }
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Login.Text = "";
+            //Server.Text = "";
+            //port.Text = "";
+            //Password.Clear();
+            SendTo.Text = "";
+            Message.Text = "";
+            Subject.Text = "";
         }
     }
 }
