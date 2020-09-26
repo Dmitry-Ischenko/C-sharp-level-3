2a67435cfcc34c8bb4fd7f3f3bfda5340ae8047d
dmitry <dmitry@dtech-ural.ru>
2020-09-26 14:22:29 +0500

    Добавил статический класс всех данных программы

---
+++ MailSender/MailSender/Data/ProgramData.cs
@@ -0,0 +1,18 @@
+﻿using MailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MailSender.Data
+{
+    static class ProgramData
+    {
+        public static ObservableCollection<Sender> SendersCollection { get; set; }
+
+        public static ObservableCollection<Recipient> RecipientsCollection { get; set; }
+
+        public static ObservableCollection<Message> MessagesCollection { get; set; }
+
+    }
+}
