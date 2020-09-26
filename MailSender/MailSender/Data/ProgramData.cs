cd138c3a44d03b41e53446fe28be074dfebb1bcd
dmitry <dmitry@dtech-ural.ru>
2020-09-26 17:03:49 +0500

    Добавил тестовые данные в объявление коллекции

--- MailSender/MailSender/Data/ProgramData.cs
+++ MailSender/MailSender/Data/ProgramData.cs
@@ -8,7 +8,27 @@ namespace MailSender.Data
 {
     static class ProgramData
     {
-        public static ObservableCollection<Sender> SendersCollection { get; set; } = new ObservableCollection<Sender>();
+        public static ObservableCollection<Sender> SendersCollection { get; set; } = new ObservableCollection<Sender>()
+        {
+            new Sender
+            {
+                Name = "Валера",
+                Address = "valera@gmail.ru",
+                Password = "test",
+                Port = 587,
+                Server = "smtp.gmail.ru",
+                UseSSl = true
+            },
+            new Sender
+            {
+                Name = "Дмитрий",
+                Address = "dmitry@test.ru",
+                Password = "test",
+                Port = 456,
+                Server = "smtp.yandex.ru",
+                UseSSl = true
+            }
+        };
 
         public static ObservableCollection<Recipient> RecipientsCollection { get; set; }
 
