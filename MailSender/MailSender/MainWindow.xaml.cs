05a94fb9b8b25af18112255d46d5f034390a2eca
dmitry <dmitry@dtech-ural.ru>
2020-09-26 22:38:10 +0500

    Доработал кнопку удаления и изменения/добавления данных в коллекцию отправителей

--- MailSender/MailSender/MainWindow.xaml.cs
+++ MailSender/MailSender/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Xml.Schema;
 using System.Windows.Media;
 using MailSender.Data;
 using MailSender.Models;
+using System.Linq;
 
 namespace MailSender
 {
@@ -24,13 +25,48 @@ namespace MailSender
         {
             if (UserSelected.SelectedItem is Sender _sender)
             {
+                int index = ProgramData.SendersCollection.IndexOf(_sender);
                 ProgramData.SendersCollection.Remove(_sender);
+                int count = ProgramData.SendersCollection.Count;
+                if (index>= count) { 
+                    index = count - 1; 
+                }
+                UserSelected.SelectedIndex = index;
             }
         }
 
         private void OnAddSenderInCollection(object sender, RoutedEventArgs e)
         {
-
+            int _port;
+            if (!Int32.TryParse(PortBox.Text,out _port))
+            {
+                return;
+            }
+            try
+            {
+                var item = ProgramData.SendersCollection.Last(i => i.Address == AddressBox.Text);
+                item.Name = NameBox.Text;
+                item.Address = AddressBox.Text;
+                item.Password = PasswordBox.Password;
+                item.Port = _port;
+                item.Server = ServerBox.Text;
+                item.UseSSl = SslBox.IsEnabled;
+                UserSelected.SelectedIndex = ProgramData.SendersCollection.IndexOf(item);
+            }
+            catch
+            {
+                Sender _sender = new Sender()
+                {
+                    Name = NameBox.Text,
+                    Address = AddressBox.Text,
+                    Password = PasswordBox.Password,
+                    Port = _port,
+                    Server = ServerBox.Text,
+                    UseSSl = SslBox.IsEnabled
+                };
+                ProgramData.SendersCollection.Add(_sender);
+                UserSelected.SelectedIndex = ProgramData.SendersCollection.Count - 1;
+            }
         }
     }
 }
