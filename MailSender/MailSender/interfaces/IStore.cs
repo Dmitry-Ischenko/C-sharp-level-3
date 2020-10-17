41090340486ee6b94820367fd648756c98b21725
dmitry <dmitry@dtech-ural.ru>
2020-10-18 00:36:17 +0500

    Реализовал доступ к таблицам бД

---
+++ MailSender/MailSender/interfaces/IStore.cs
@@ -0,0 +1,20 @@
+﻿using MailClient.lib.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.interfaces
+{
+    public interface IStore<T> where T : ModelBase
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int Id);
+
+        T Add(T Item);
+
+        void Update(T Item);
+
+        void Delete(int Id);
+    }
+}
